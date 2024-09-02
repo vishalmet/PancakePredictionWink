2e72ed1fe58f5eac0111c346ea324108d8b8c86e
vishalmet <vishalmet003@gmail.com>
2024-09-02 22:31:59 +0530

    handleflip

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ const Home = ({ handleFlip }) => {
   return (
     <div>
       <div className="flex justify-center items-center pt-10">
-        <div className="bg-customStart1/10 shadow-customButtonStroke/50 shadow-xl h-72 rounded-xl mx-6 md:mx-0 w-[400px] md:w-[500px] space-y-8">
+        <div className="bg-customStart1/10 border-2 border-customStart1/20 shadow-xl h-72 rounded-xl mx-6 md:mx-0 w-[400px] md:w-[500px] space-y-8">
           <div className=" bg-customButtonStroke text-white font-bold text-2xl p-3 flex justify-center rounded-t-xl">
             Pancake Prediction
           </div>
@@ -29,7 +29,7 @@ const Home = ({ handleFlip }) => {
                   <motion.button
                     whileTap={{ scale: 0.9 }}
                     className="text-white text-xl shadow-blue-950 hover:shadow-2xl w-[280px] md:w-[450px] h-[48px] bg-red-500 font-bold hover:bg-red-600 rounded-xl"
-                    onClick={() => handleFlip("BetBeer")}
+                    onClick={() => handleFlip("BetBear")}
                   >
                     Enter DOWN
                   </motion.button>
