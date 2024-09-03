084b213b01740300c3b658c2c4034df131b85262
vishalmet <vishalmet003@gmail.com>
2024-09-03 21:58:41 +0530

    ui update

--- src/components/BetBull.jsx
+++ src/components/BetBull.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
-import { initializeContract, betBull } from '../integration';
+import BNBLogo from "../assets/busd.png";
+import { ethers } from "ethers";
+import { initializeContract, betBull, currentEpoch } from "../integration";
 
 const BetBull = ({ handleFlip }) => {
   const [provider, setProvider] = useState(null);
@@ -9,17 +11,22 @@ const BetBull = ({ handleFlip }) => {
   const [contract, setContract] = useState(null);
   const [value, setValue] = useState("");
   const [showWarning, setShowWarning] = useState(false);
+  const [actualEpoch, setActualEpoch] = useState(""); // State for displaying the epoch
 
   useEffect(() => {
     async function initialize() {
       try {
-        const { provider, signer, address, contract } = await initializeContract();
+        const { provider, signer, address, contract } =
+          await initializeContract();
         setProvider(provider);
         setSigner(signer);
         setAddress(address);
         setContract(contract);
+
+        const epoch = await contract.currentEpoch();
+        setActualEpoch(epoch.toString()); // Convert and set for display
       } catch (error) {
-        console.error('Error initializing contract:', error);
+        console.error("Error initializing contract:", error);
       }
     }
     initialize();
@@ -39,19 +46,21 @@ const BetBull = ({ handleFlip }) => {
   const handleBetBull = async () => {
     if (contract && signer && parseFloat(value) >= 0.001) {
       try {
-        await betBull( value);
+        // Use BigNumber directly from the state for the transaction
+        const epochForTransaction = ethers.BigNumber.from(actualEpoch);
+        await betBull(value, epochForTransaction);
       } catch (error) {
-        console.error('Error executing betBull:', error);
+        console.error("Error executing betBull:", error);
       }
     } else {
-      console.warn('Invalid bet or contract not initialized');
+      console.warn("Invalid bet or contract not initialized");
     }
   };
 
   return (
     <div className="flex justify-center items-center pt-10">
-      <div className="bg-customStart1/10 shadow-green-500/50 shadow-xl h-fit rounded-xl mx-6 md:mx-0 w-[400px] md:w-[500px] space-y-8 pb-8">
-        <div className=" bg-customStart1/20 text-white font-bold text-2xl p-3 flex justify-between rounded-t-xl">
+      <div className="bg-[#27262C] shadow-xl h-full rounded-xl mx-6 md:mx-0 w-[400px] md:w-[500px]">
+        <div className="bg-[#3A384C] text-white font-bold p-3 flex justify-between rounded-t-xl">
           <div className="flex items-center gap-3">
             <motion.button
               whileTap={{ scale: 0.9 }}
@@ -74,47 +83,54 @@ const BetBull = ({ handleFlip }) => {
             </motion.button>
             <p>Set Position</p>
           </div>
-          <div className=" flex justify-center text-base gap-2 items-center bg-green-500 p-2 rounded-md">
-            UP{" "}
+          {/* <p className="text-sm"># {actualEpoch}</p> */}
+          <div className=" flex items-center bg-[#31D0AA] p-1 px-3 rounded-lg">
+            UP
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              fill="none"
               viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="size-5"
+              fill="currentColor"
+              class="size-5"
             >
               <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M4.5 10.5L12 3m0 0l7.5 7.5M12 3v18"
+                fill-rule="evenodd"
+                d="M11.47 2.47a.75.75 0 0 1 1.06 0l7.5 7.5a.75.75 0 1 1-1.06 1.06l-6.22-6.22V21a.75.75 0 0 1-1.5 0V4.81l-6.22 6.22a.75.75 0 1 1-1.06-1.06l7.5-7.5Z"
+                clip-rule="evenodd"
               />
             </svg>
           </div>
         </div>
-        <div className="mx-5 space-y-4">
-          <div className=" flex justify-between font-semibold text-xl">
-            <p>Commit:</p>
-            <p>0.0050 BNB</p>
+        <div className="mx-5 space-y-2 p-4">
+          <div className=" flex justify-between font-bold text-xl">
+            <p className=" text-[#B0A5C9] font-medium">Commit:</p>
+            <p className=" flex items-center">
+              {" "}
+              <img className=" h-8 w-8" src={BNBLogo} alt="" /> BNB
+            </p>
           </div>
-
           <input
             type="text"
             placeholder="0.0"
             value={value}
             onChange={handleInputChange}
-            className="bg-customStart1/10 border font-bold border-customGrayStroke rounded-md w-full h-12 px-3 text-right"
+            className="bg-[#372F47] border font-bold border-customGrayStroke/50 rounded-md w-full h-12 px-3 text-right"
           />
           {showWarning && (
-            <p className="text-red-500 text-sm">The minimum value should be greater than 0.0001.</p>
+            <p className="text-red-500 text-sm">
+              The minimum value should be greater than 0.0001.
+            </p>
           )}
           <motion.button
             onClick={handleBetBull}
             whileTap={{ scale: 0.9 }}
-            className=" bg-green-500 hover:bg-green-600 w-full h-12 rounded-md text-xl font-bold mb-3"
+            className=" bg-[#31D0AA] text-white w-full h-12 rounded-md text-xl font-bold mb-3"
           >
-            Predict
+            Confirm
           </motion.button>
+          <p className="text-[#B0A5C9] text-sm font-semibold text-center">
+            You won&apos;t be able to remove or change <br /> your position once
+            you enter it.
+          </p>
         </div>
       </div>
     </div>
