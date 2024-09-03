37e44da5dbca22d03a41b8f53067c8460c40ba63
vishalmet <vishalmet003@gmail.com>
2024-09-03 13:48:15 +0530

    wink adjustment

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ const App = () => {
 
   return (
     <div className="flex justify-center items-center min-h-screen bg-gradient-to-t from-customStart via-customStart to-blue-950">
-      <div className="flex flex-col items-center mx-auto bg-gradient-to-t from-customStart via-customStart to-blue-950 min-h-screen text-white pt-20">
+      <div className="flex flex-col items-center mx-auto bg-gradient-to-t from-customStart via-customStart to-blue-950 min-h-screen text-white pt-12">
         <div className="flex justify-center">
           <div className=" space-y-3">
             <img className="h-10 w-auto mx-auto" src={HeaderLogo} alt="Header Logo" />
