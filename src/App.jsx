9361a177d405f03de231ae1aea897114e6c401d9
vishalmet <vishalmet003@gmail.com>
2024-09-04 10:31:16 +0530

    wink ui

--- src/App.jsx
+++ src/App.jsx
@@ -22,46 +22,50 @@ const App = () => {
       <div className="flex flex-col items-center mx-auto min-h-screen text-white pt-6">
         <div className="flex justify-center">
           <div className=" space-y-3">
-            <img className="h-6 w-auto mx-auto" src={HeaderLogo} alt="Header Logo" />
+            <img
+              className="h-6 w-auto mx-auto"
+              src={HeaderLogo}
+              alt="Header Logo"
+            />
             <Wallet />
           </div>
         </div>
-        <div className=" flex ">
+        <div className="flex gap-3">
           <LiveUp />
-        <div className="relative w-[280px] perspective">
-          <motion.div
-            className="relative w-full h-full"
-            style={{ transformStyle: "preserve-3d" }}
-            initial={{ rotateY: 0 }}
-            animate={{ rotateY: isFlipped ? 180 : 0 }}
-            transition={{ duration: 0.8 }}
-          >
-            {/* Front Side */}
-            <div
-              className="absolute w-full h-full"
-              style={{
-                backfaceVisibility: "hidden",
-              }}
+          <div className="relative w-[240px] perspective">
+            <motion.div
+              className="relative w-full h-full"
+              style={{ transformStyle: "preserve-3d" }}
+              initial={{ rotateY: 0 }}
+              animate={{ rotateY: isFlipped ? 180 : 0 }}
+              transition={{ duration: 0.8 }}
             >
-              <Home handleFlip={handleFlip} />
-            </div>
+              {/* Front Side */}
+              <div
+                className="absolute w-full h-full"
+                style={{
+                  backfaceVisibility: "hidden",
+                }}
+              >
+                <Home handleFlip={handleFlip} />
+              </div>
 
-            {/* Back Side */}
-            <div
-              className="absolute w-full h-full"
-              style={{
-                transform: "rotateY(180deg)",
-                backfaceVisibility: "hidden",
-              }}
-            >
-              {backComponent === "BetBull" && (
-                <BetBull handleFlip={handleFlip} />
-              )}
-              {backComponent === "BetBear" && (
-                <BetBeer handleFlip={handleFlip} />
-              )}
-            </div>
-          </motion.div>
+              {/* Back Side */}
+              <div
+                className="absolute w-full h-full"
+                style={{
+                  transform: "rotateY(180deg)",
+                  backfaceVisibility: "hidden",
+                }}
+              >
+                {backComponent === "BetBull" && (
+                  <BetBull handleFlip={handleFlip} />
+                )}
+                {backComponent === "BetBear" && (
+                  <BetBeer handleFlip={handleFlip} />
+                )}
+              </div>
+            </motion.div>
           </div>
         </div>
       </div>
