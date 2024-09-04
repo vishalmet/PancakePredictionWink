3c716ce8046d835ec48d3edc1d2ce0532dff2c29
vishalmet <vishalmet003@gmail.com>
2024-09-04 10:14:38 +0530

    text size

--- src/components/BetBear.jsx
+++ src/components/BetBear.jsx
@@ -56,7 +56,7 @@ const BetBear = ({ handleFlip }) => {
     <div className="flex justify-center items-center pt-10">
       <div className="bg-[#27262C] shadow-xl h-full rounded-3xl mx-6 md:mx-0 w-[280px]">
         <div className="bg-[#3A384C] text-white font-bold p-3 px-5 flex justify-between rounded-t-3xl">
-          <div className="flex items-center gap-3">
+          <div className="flex items-center gap-1">
             <motion.button
               whileTap={{ scale: 0.9 }}
               onClick={() => handleFlip(null)}
@@ -117,7 +117,7 @@ const BetBear = ({ handleFlip }) => {
           <motion.button
             onClick={handleBetBear}
             whileTap={{ scale: 0.9 }}
-            className=" bg-[#ED4B9E] hover:bg-[#ED4B9E]/80 text-white w-full h-12 rounded-md text-xl font-bold mb-3"
+            className=" bg-[#ED4B9E] hover:bg-[#ED4B9E]/80 text-white w-full h-[40px] text-base rounded-md font-bold mb-3"
           >
             Confirm
           </motion.button>
