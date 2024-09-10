fc4eff92c95be5cfe95ddfaa8e7c937f69a48356
vishalmet <vishalmet003@gmail.com>
2024-09-10 09:13:31 +0530

    animate-border-pulse

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,21 @@ module.exports = {
       },
       boxShadow: {
         'custom': '0 0 15px 5px rgba(168, 129, 252, 0.3)'
-      }
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      keyframes: {
+        borderColorChange: {
+          '0%, 100%': { borderColor: '#A881FC' }, // starting color
+          '50%': { borderColor: '#1FC7D4' }, // mid color
+        },
+      },
+      animation: {
+        'border-pulse': 'borderColorChange 2s infinite', // animation name and duration
+      },
     },
   },
   variants: {},
